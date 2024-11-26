6efe7b028d8abc32f97d9cf356950a675305ae6b
= <nathaniel.risum2@gmail.com>
2024-11-26 10:54:03 +0100

    Setup some test-variables

--- C#/FruitREST/Program.cs
+++ C#/FruitREST/Program.cs
@@ -1,3 +1,5 @@
+using FruitREST;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,12 +11,15 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+#if DEBUG
+TestMode.TestModeIsDev = true;
+#endif
+
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
