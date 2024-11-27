b7d53b2ad36c7f7a9cd40c7f712b15d5e4d3972c
ZombieFreak115 <knudsenmarius@gmail.com>
2024-11-27 15:13:03 +0100

    fixed some logic issues and added exttra error info on wrong status code. Always returns 400 bad request. To be resolved

--- Python/ProxyServer.py
+++ Python/ProxyServer.py
@@ -7,7 +7,8 @@ import datetime
 
 serverPort = 727
 
-destinationURL = "Placeholder"
+#TODO: Change placeholder to real destination IP addr
+destinationURL = "https://localhost:7165/api/readings"
 
 serverName = "0.0.0.0"
 
@@ -26,13 +27,14 @@ while True:
         message, _ = serverSocket.recvfrom(15441600)
         messageAsString = message.decode()
         messageAsDictionary = json.loads(messageAsString)
-        if "temperature" not in messageAsDictionary or type(messageAsDictionary["temperature"] != type(float)):
+        if "temperature" not in messageAsDictionary or (type(messageAsDictionary["temperature"]) is not float and type(messageAsDictionary["temperature"]) is not int):
             raise KeyError("temperature was not included in the JSON, or has an invalid value")
-        elif "humidity" not in messageAsDictionary or type(messageAsDictionary["humidity"] != type(float)):
+        elif "humidity" not in messageAsDictionary or (type(messageAsDictionary["humidity"]) is not float and type(messageAsDictionary["humidity"]) is not int):
             raise KeyError("humidity was not included in the JSON, or has an invalid value")
-        response = requests.post(destinationURL, json=messageAsString)
+        # TODO: Remove "verify=False" if we can make it work without
+        response = requests.post(destinationURL, json=messageAsString, verify=False)
         if response.status_code != 201:
-            writeLogEntry(datetime.datetime.now(), "WRONG STATUS CODE", "Status code was " + response.status_code )
+            writeLogEntry(datetime.datetime.now(), "WRONG STATUS CODE", "Status code was " + str(response.status_code) + " RESPONSE TEXT: " + response.text )
             
     except Exception as e:
         # do stuff if there is an error
