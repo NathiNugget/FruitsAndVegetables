dae36ae538558b41bf594cbe73385ba0351c7b9a
Jacob Sejer Kristoffersen <jak002@edu.zealand.dk>
2024-11-28 13:58:34 +0100

    Selenium can now access vue site

--- C#/UITest/UItestfrontend.cs
+++ C#/UITest/UItestfrontend.cs
@@ -10,7 +10,7 @@ namespace UITest
     {
         IWebDriver driver;
         // TODO: Replace URL when running tests
-        const string TEST_URL = "http://127.0.0.1:5500/";
+        const string TEST_URL = "http://127.0.0.1:5500/Vue";
         const int _maxWaitMillis = 500; 
 
         [TestInitialize]
@@ -46,18 +46,6 @@ namespace UITest
         }
 
         [TestMethod]
-        public void SelectFood()
-        {
-            IWebElement dropdown = driver.FindElement(By.Id("FoodDropdown"));
-            dropdown.Click();
-            IWebElement selectedFood = driver.FindElement(By.Id("Banan"));
-            selectedFood.Click();
-            Thread.Sleep(_maxWaitMillis);
-            IWebElement spoilTime = driver.FindElement(By.Id("SpoilTime"));
-            Assert.IsNotNull(spoilTime);
-        }
-
-        [TestMethod]
         public void SelectFoodCorrectName()
         {
             IWebElement dropdown = driver.FindElement(By.Id("FoodDropdown"));
@@ -86,6 +74,19 @@ namespace UITest
             IWebElement selectedElement = driver.FindElement(By.Id("SelectedName")); //TODO: Change Placeholder ID to actual element
             string actual = selectedElement.Text;
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShelfLifeNotNull()
+        {
+            IWebElement dropdown = driver.FindElement(By.Id("FoodDropdown"));
+            dropdown.Click();
+            SelectElement selectElement = new SelectElement(dropdown);
+            string selectedFood = "Banan";
+            selectElement.SelectByValue(selectedFood);
+            Thread.Sleep(_maxWaitMillis);
+            IWebElement shelfLife = driver.FindElement(By.Id("ShelfLife"));
+            Assert.IsNotNull(shelfLife);
         }
 
         [TestMethod]
@@ -119,7 +120,7 @@ namespace UITest
         }
 
         [TestMethod]
-        public void FruitsOnly()
+        public void FruitsOnly_InvalidVegetable()
         {
             IWebElement fruitCheckbox = driver.FindElement(By.Id("FruitCheck"));
             fruitCheckbox.Click();
@@ -130,9 +131,21 @@ namespace UITest
             IWebElement? CheckedElement = options.FirstOrDefault(v => v.Text == "Agurk");
             Assert.IsNull(CheckedElement);
         }
+        [TestMethod]
+        public void FruitsOnly_ValidFruit()
+        {
+            IWebElement fruitCheckbox = driver.FindElement(By.Id("FruitCheck"));
+            fruitCheckbox.Click();
+            IWebElement dropdown = driver.FindElement(By.Id("FoodDropdown"));
+            dropdown.Click();
+            SelectElement selectElement = new SelectElement(dropdown);
+            IList<IWebElement> options = selectElement.Options;
+            IWebElement? CheckedElement = options.FirstOrDefault(v => v.Text == "Banan");
+            Assert.IsNotNull(CheckedElement);
+        }
 
         [TestMethod]
-        public void VegetablesOnly()
+        public void VegetablesOnly_InvalidFruit()
         {
             IWebElement vegetableCheckbox = driver.FindElement(By.Id("VegetableCheck"));
             vegetableCheckbox.Click();
@@ -142,6 +155,19 @@ namespace UITest
             IList<IWebElement> options = selectElement.Options;
             IWebElement? CheckedElement = options.FirstOrDefault(v => v.Text == "Banan");
             Assert.IsNull(CheckedElement);
+        }
+
+        [TestMethod]
+        public void VegetablesOnly_ValidVegetable()
+        {
+            IWebElement vegetableCheckbox = driver.FindElement(By.Id("VegetableCheck"));
+            vegetableCheckbox.Click();
+            IWebElement dropdown = driver.FindElement(By.Id("FoodDropdown"));
+            dropdown.Click();
+            SelectElement selectElement = new SelectElement(dropdown);
+            IList<IWebElement> options = selectElement.Options;
+            IWebElement? CheckedElement = options.FirstOrDefault(v => v.Text == "Agurk");
+            Assert.IsNotNull(CheckedElement);
         }
 
         [TestCleanup]
