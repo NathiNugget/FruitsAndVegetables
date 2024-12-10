230aed299c7eeb7674d0eed2bbaedfe1f018afb6
= <nathaniel.risum2@gmail.com>
2024-12-10 15:40:35 +0100

    Latest progression

--- Python/ProxyServer.py
+++ Python/ProxyServer.py
@@ -3,13 +3,17 @@ import threading
 import json
 import requests
 import datetime
+import paramiko
+import os
+
 
 headers = {"Content-Type": "application/json"}
 
 serverPort = 727
 
 #TODO: Change placeholder to real destination IP addr
-destinationURL = "https://fruitcontainerproduction.azurewebsites.net/api/Readings"
+destinationReadingURL = "https://fruitcontainerproduction.azurewebsites.net/api/Readings"
+destinationImageURL = "https://fruitcontainerproduction.azurewebsites.net/api/Images"
 
 serverName = "0.0.0.0"
 
@@ -25,20 +29,43 @@ def writeLogEntry(timeStamp, prefix, text):
 
 while True:
     try:
-        message, _ = serverSocket.recvfrom(15441600)
+        message, returnAddress = serverSocket.recvfrom(15441600)
         messageAsString = message.decode()
         messageAsDictionary = json.loads(messageAsString)
         if "temperature" not in messageAsDictionary or (type(messageAsDictionary["temperature"]) is not float and type(messageAsDictionary["temperature"]) is not int):
             raise KeyError("temperature was not included in the JSON, or has an invalid value")
         elif "humidity" not in messageAsDictionary or (type(messageAsDictionary["humidity"]) is not float and type(messageAsDictionary["humidity"]) is not int):
             raise KeyError("humidity was not included in the JSON, or has an invalid value")
-        # TODO: Remove "verify=False" if we can make it work without
-        response = requests.post(destinationURL, data=messageAsString, headers=headers, verify=False)
+
+
+        
+
+        response = requests.post(destinationReadingURL, data=messageAsString, headers=headers)
+        
+        
+        client = paramiko.SSHClient()
+        #client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        client.load_host_keys(os.getcwd()+"\\known_hosts")
+        print(client.get_host_keys().keys())
+        client.connect(f'{returnAddress[0]}', port=22, username='pi', password='raspberry')
+        print(client.get_host_keys())
+        SFTP = client.open_sftp()
+        SFTP.get("/home/pi/Nathaniel/newest_image.jpg", "newest_image.jpg")
+        SFTP.close()
+        client.close()    
+            
+
+
+
+
+
         if response.status_code != 201:
             writeLogEntry(datetime.datetime.now(), "WRONG STATUS CODE", "Status code was " + str(response.status_code) + " RESPONSE TEXT: " + response.text )
-            
+
     except Exception as e:
         # do stuff if there is an error
         writeLogEntry(datetime.datetime.now(), "SENSOR PEER ERROR", repr(e) )
+        print("Something went wrong, Nath!")
         continue
     
