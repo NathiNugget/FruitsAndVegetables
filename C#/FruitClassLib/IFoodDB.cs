be3216d735b17c37920da57969da9d51171451df
ZombieFreak115 <knudsenmarius@gmail.com>
2024-12-11 12:49:51 +0100

    Changed the post method for foods to require a valid token, only if it is in non-test mode

--- C#/FruitClassLib/IFoodDB.cs
+++ C#/FruitClassLib/IFoodDB.cs
@@ -3,7 +3,7 @@ namespace FruitClassLib
 {
     public interface IFoodDB
     {
-        Food Add(Food food);
+        Food Add(Food food, string? token = null);
         Food FindByName(string name);
         List<Food> GetAllFiltered(bool? filterFruit = null, bool? filterVegetable = null, string? nameFilter = null, int? offset = null, int? count = null);
 
