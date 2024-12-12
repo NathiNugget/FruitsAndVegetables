547e9f3323b1b492ca60f403ab95c837a2d7e28c
= <nathaniel.risum2@gmail.com>
2024-12-12 09:31:07 +0100

    Remove old and unneccessary comments from Image-script

--- Python/ReadBytes.py
+++ Python/ReadBytes.py
@@ -8,20 +8,3 @@ def TakeImage():
     cam = Camera()
     cam.still_size = (4608,2592)
     cam.take_photo(path)
-
-# def GetDataToSend(): 
-#     with open(path, "rb") as image_file:
-#         data = base64.b64encode(image_file.read())
-#         dataDict = {"bytes": str(data)}
-#         return dataDict
-
-# def SnapAndSend():
-#     TakeImage()
-#     bytes = GetDataToSend()
-#     return bytes
-
-
-# im = Image.open(BytesIO(base64.b64decode(data)))
-# im.save('after.png', 'PNG')
-
-
