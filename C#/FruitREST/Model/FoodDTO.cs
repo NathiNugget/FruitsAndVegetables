524b59f140dac2db893ebeb7efcd8b023c6073b0
Marius Egede Knudsen <mak004@edu.zealand.dk>
2024-12-12 11:12:52 +0100

    refactoed q10 to be double, among other minor refactorings

--- C#/FruitREST/Model/FoodDTO.cs
+++ C#/FruitREST/Model/FoodDTO.cs
@@ -1,7 +1,7 @@
 ﻿namespace FruitREST.Model
 {
 
-    public record FoodDTO(string name, int foodTypeId, string apiLink, byte spoilDate, byte spoilHours, double idealTemperature, double idealHumidity,  string foodTypeName = "", byte q10Factor = 3, double maxTemp = 40, double minTemp = -15);
+    public record FoodDTO(string name, int foodTypeId, string apiLink, byte spoilDate, byte spoilHours, double idealTemperature, double idealHumidity,  string foodTypeName = "", double q10Factor = 3, double maxTemp = 40, double minTemp = -15);
 
 
 
